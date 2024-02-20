62cda91799e8675667a5cc32b94ad1855e093f8c
Alexander Pogorelov <ics.price@gmail.com>
2024-02-20 13:10:18 +0300

    removed the trait and interface

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -4,7 +4,33 @@ declare(strict_types=1);
 
 namespace AlexanderPogorelov\DateTimeHelper;
 
-class DateHelper implements DateHelperInterface
+class DateHelper
 {
-    use DateHelperTrait;
+    public const MYSQL_STRING_FORMAT = 'Y-m-d H:i:s';
+
+    /**
+     * @throws \Exception
+     */
+    public function getCurrentDateMySQL(string $timezone = 'UTC'): string
+    {
+        return (new \DateTimeImmutable('now', new \DateTimeZone($timezone)))->format(self::MYSQL_STRING_FORMAT);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function createDateFromString(string $datetime, string $timezone = 'UTC'): \DateTimeImmutable
+    {
+        $date = \DateTimeImmutable::createFromFormat(
+            self::MYSQL_STRING_FORMAT,
+            $datetime,
+            new \DateTimeZone($timezone)
+        );
+
+        if (!$date instanceof \DateTimeImmutable) {
+            throw new \Exception(sprintf('Unable to create date from string %s with timezone %s.', $datetime, $timezone));
+        }
+
+        return $date;
+    }
 }
